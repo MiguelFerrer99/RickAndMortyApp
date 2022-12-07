444b6600930a35cf1dfd84bee8266e3097e1e862
Miguel Ferrer Fornali <mferrerf@ext.vectoritcgroup.com>
2022-12-07 11:44:28 +0100

    FIX: Fix titleView animation in all devices

--- RickAndMorty/RickAndMorty/Presentation/Scenes/Home/View/DataView/HomeDataView.swift
+++ RickAndMorty/RickAndMorty/Presentation/Scenes/Home/View/DataView/HomeDataView.swift
@@ -16,7 +16,9 @@ final class HomeDataView: XibView {
     @IBOutlet private weak var titleView: UIView!
     @IBOutlet private weak var topImageView: UIImageView!
     @IBOutlet private weak var topImageViewDefaultHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var topImageViewDefaultWidthConstraint: NSLayoutConstraint!
     @IBOutlet private weak var topImageViewReceivedDataHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var topImageViewReceivedWidthConstraint: NSLayoutConstraint!
     @IBOutlet private weak var collectionView: HomeDataCollectionView!
     
     private var subscriptions = Set<AnyCancellable>()
@@ -52,10 +54,12 @@ private extension HomeDataView {
     }
     
     func moveImageToTop() {
-        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 20, initialSpringVelocity: 5) { [weak self] in
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 20, initialSpringVelocity: 5) { [weak self] in
             guard let self = self else { return }
             self.topImageViewDefaultHeightConstraint.isActive = false
+            self.topImageViewDefaultWidthConstraint.isActive = false
             self.topImageViewReceivedDataHeightConstraint.isActive = true
+            self.topImageViewReceivedWidthConstraint.isActive = true
             self.layoutIfNeeded()
         } completion: { [weak self] finished in
             guard let self = self else { return }
@@ -73,7 +77,7 @@ private extension HomeDataView {
     }
     
     func showCollecionView() {
-        UIView.animate(withDuration: 0.35, delay: 0.25) { [weak self] in
+        UIView.animate(withDuration: 0.3, delay: 0) { [weak self] in
             guard let self = self else { return }
             self.collectionView.alpha = 1
         } completion: { [weak self] finished in
